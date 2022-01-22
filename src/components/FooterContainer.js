fb4d8dedf467df6ee85ec2b991ed0719fcaeb06f
ESAM-ALSAWAH <esam.alsawah2000@gmail.com>
2022-01-22 20:07:58 +0300

    finished home page

--- src/components/FooterContainer.js
+++ src/components/FooterContainer.js
@@ -2,13 +2,57 @@ import { Row, Col, Typography, Button } from 'antd'
 import React from 'react'
 import { ArrowRightOutlined } from '@ant-design/icons'
 import shapeScreen from '../assets/doodles.png'
+import Logo from '../assets/icons/logo.svg'
 const { Title, Text } = Typography
+
+const listData = {
+  col1: {
+    title: 'Home',
+    data: [
+      'About',
+      'Contact Us',
+      'Faqs',
+      'Our Services',
+      'Our Team',
+      'Payment',
+    ],
+  },
+  col2: {
+    title: 'Sitemap',
+    data: ['About', 'What We Do', 'Projections', 'Location', 'Purchase?'],
+  },
+  col3: {
+    title: 'Discover',
+    data: ['Privacy Policy', 'Terms & Conditions', 'Owner Portal', 'Houses'],
+  },
+}
+
 const Column = ({ children }) => (
   <Col lg={{ span: 12 }} md={{ span: 12 }} sm={{ span: 24 }} xs={{ span: 24 }}>
     {children}
   </Col>
 )
 export const FooterContainer = () => {
+  const renderList = Object.values(listData).map(({ title, data }, index) => {
+    return (
+      <Col
+        lg={{ span: 8 }}
+        md={{ span: 8 }}
+        sm={{ span: 24 }}
+        xs={{ span: 24 }}
+        key={index}
+      >
+        <Title className="title" level={3}>
+          {title}
+        </Title>
+        {data.map((value, index) => (
+          <Text className="text" key={index}>
+            {value}
+          </Text>
+        ))}
+      </Col>
+    )
+  })
   return (
     <Row className="relative">
       <Col className="col-1">
@@ -24,8 +68,20 @@ export const FooterContainer = () => {
           <img src={shapeScreen} className="img img-2" />
         </Row>
       </Col>
-      <Col>
-        <Row className="container"></Row>
+      <Col className="col-2">
+        <Row className="container">
+          <Column>
+            <div className="icon-container">
+              <i className="ri-facebook-fill"></i>
+              <i className="ri-twitter-fill"></i>
+              <i className="ri-youtube-fill"></i>
+              <i className="ri-instagram-line"></i>
+            </div>
+          </Column>
+          <Column>
+            <Row className="list-container">{renderList}</Row>
+          </Column>
+        </Row>
       </Col>
     </Row>
   )
